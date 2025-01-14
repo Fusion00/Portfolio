5b2639cd1ca4ac0daf612967ead77332a1bde8f3
Dhruv <dhruvkr7209@gmail.com>
2025-01-14 19:09:45 +0530

    tech

--- src/Components/Tech.tsx
+++ src/Components/Tech.tsx
@@ -4,7 +4,20 @@ const Tech = () => {
   return (
     <div>
       <div className="flex flex-wrap bg-gradient-to-b from-[#001a1a] from-10% via-[#032125] via-50% to-[#0c3b45] to-90%  justify-center py-32 gap-24 font-rounded">
-        <div></div>
+        <div className="bg-[#00BEB9] h-10 w-[432px] px-8 py-2 rounded-md flex justify-center">Technologies</div>
+        <div className="w-auto px-10 flex flex-wrap gap-10 justify-center items-baseline">
+            <img src="html.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+            <img src="css.png" alt="html" className="h-14 w-15 md:h-24 md:w-22"/>
+            <img src="js.png" alt="html" className="h-12 w-12 md:h-24 md:w-24"/>
+            <img src="react.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+            <img src="next.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+            <img src="typescript.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+            <img src="node.png" alt="html" className="h-14 w-13 md:h-24 md:w-23"/>
+            <img src="firebase.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+            <img src="Mongodb.png" alt="html" className="h-14 w-25 md:h-24 md:w-38"/>
+            <img src="sql.png" alt="html" className="h-14 w-20 md:h-24 md:w-36"/>
+            <img src="kubernetes.png" alt="html" className="h-14 w-14 md:h-24 md:w-24"/>
+        </div>
       </div>
     </div>
   )
