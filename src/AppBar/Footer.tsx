61d3dc908aada4e8d709167c94025b27e75172bf
Dhruv <dhruvkr7209@gmail.com>
2025-01-15 21:27:29 +0530

    footer hide

--- src/AppBar/Footer.tsx
+++ src/AppBar/Footer.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
+  const navigate = useNavigate()
     return (
       <div>
         <div className="relative flex justify-center font-rounded text-[#001A1A] bg-[#00BEB9] py-48 sm:text-3xl text-xl">
           <div className="absolute top-20">
             <p className="text-center">I Build fast, sleek, modern websites.</p>
           </div>
+          <div className="h-2 w-20"
+          onClick={() => navigate("/Backoffice")}></div>
         </div>
   
   
