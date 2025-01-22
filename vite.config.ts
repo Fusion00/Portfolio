ecf7c4c5a7ee4fd56dd00f7260be5940c924b829
Dhruv <dhruvkr7209@gmail.com>
2025-01-22 23:43:04 +0530

    vite.config back

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"./",
+  base:"./Portfolio",
   resolve: {
     alias: {
       'remixicon.css': 'remixicon/fonts/remixicon.css'
